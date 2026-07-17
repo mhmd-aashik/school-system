41a475c270eb09650888c0df2a9e099dc18f1b99
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-17 17:46:27 +0530

    Enhance gRPC integration and update environment configurations
    - Added gRPC host and port to the .env file for service communication.
    - Updated package.json and package-lock.json to include @grpc/grpc-js and @grpc/proto-loader dependencies for gRPC support.
    - Modified main.ts in the student service to create a gRPC microservice with appropriate transport settings.
    - Updated tsconfig.json to include the path for the new proto library.
    - Cleaned up app.module.ts in the api-gateway by removing unused controllers and providers.

--- apps/student-service/src/main.ts
+++ apps/student-service/src/main.ts
@@ -6,11 +6,21 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { StudentModule } from './app/student.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(StudentModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    StudentModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'student',
+        protoPath: join(process.cwd(), 'libs/proto/src/lib/student.proto'),
+        url: 'localhost:50051',
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,11 +30,9 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  await app.listen();
+
+  Logger.log('🚀 Student gRPC Service is running on localhost:50051');
 }
 
 bootstrap();
